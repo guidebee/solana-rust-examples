d06b8b4a04d3c641a7814f3f42162f59e363ade7
James Shen <james.shen@guidebee.com>
2024-12-26 19:07:23 +0800

    add redis example

--- src/bin/redis_client.rs
+++ src/bin/redis_client.rs
@@ -1,16 +1,25 @@
+use dotenv::dotenv;
 use redis::aio::MultiplexedConnection;
 use redis::{AsyncCommands, Client};
 use std::sync::Arc;
-use tokio::sync::Mutex;
 use tokio;
+use tokio::sync::Mutex;
+
+#[derive(serde::Deserialize)]
+struct Env {
+    wallet_pubkey: String,
+}
 
 async fn get_redis_client_with_retry() -> redis::RedisResult<Arc<Mutex<MultiplexedConnection>>> {
-    let client  = Client::open("redis://10.1.1.150:6379")?;
+    let client = Client::open("redis://10.1.1.150:6379")?;
     let conn = client.get_multiplexed_async_connection().await?;
     Ok(Arc::new(Mutex::new(conn)))
 }
 
-async fn get_balance(client: Arc<Mutex<MultiplexedConnection>>, wallet_address: &str) -> redis::RedisResult<String> {
+async fn get_balance(
+    client: Arc<Mutex<MultiplexedConnection>>,
+    wallet_address: &str,
+) -> redis::RedisResult<String> {
     let balance_key = "balance";
     let mut conn = client.lock().await;
     let stored_balances_str: String = conn.hget(balance_key, wallet_address).await?;
@@ -19,26 +28,18 @@ async fn get_balance(client: Arc<Mutex<MultiplexedConnection>>, wallet_address: 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let env = envy::from_env::<Env>()?;
     let client = get_redis_client_with_retry().await?;
 
-    let wallet_address = "your_wallet_address_in_base58";
+    let wallet_address = env.wallet_pubkey;
 
-    loop {
-        match get_balance(client.clone(), wallet_address).await {
-            Ok(balance) => {
-                println!("Stored Balances String: {}", balance);
-                break;
-            }
-            Err(err) => {
-                // if err.is::<redis::RedisError>() && err.as_ref().unwrap().kind() == ErrorKind::ConnectionReset {
-                //     eprintln!("Connection lost, retrying in 5 seconds: {:?}", err);
-                //     tokio::time::sleep(Duration::from_secs(5)).await;
-                // } else {
-                //     eprintln!("Error fetching balance: {:?}", err);
-                //     break;
-                // }
-                eprintln!("Error fetching balance: {:?}", err);
-            }
+    match get_balance(client.clone(), &wallet_address[..]).await {
+        Ok(balance) => {
+            println!("Stored Balances String: {}", balance);
+        }
+        Err(err) => {
+            eprintln!("Error fetching balance: {:?}", err);
         }
     }
 
